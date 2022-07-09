67fcb2a9991d85a7035972e7e241d48e37717889
ksushakropachova <102968014+ksushakropachova@users.noreply.github.com>
2022-07-09 21:12:16 +0300

    Added files

---
+++ index.js
@@ -0,0 +1,19 @@
+// function countpeople() {
+//     let handshake = 0;
+//     let people = 1;
+
+//     while (handshake == 120){
+//         people += 1;
+//         handshake += (people - 1);
+//     }
+//     console.log(people);
+// }
+
+let people = 1; 
+let handshake = 0;
+
+while (handshake != 120){
+    people += 1;
+    handshake += (people - 1);
+}
+console.log(people);
